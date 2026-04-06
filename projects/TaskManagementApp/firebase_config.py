f42cf49981cd86000a286cd92ecd8ede62fc4e42
jrichwiltshire <jrichwiltshire@gmail.com>
2026-04-06 00:14:38 -0500

    working on Phase 1 of the Shopping List-Task Manager App: the Shopping List

--- projects/TaskManagementApp/firebase_config.py
+++ projects/TaskManagementApp/firebase_config.py
@@ -1,6 +1,8 @@
-import os
+import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("firebase_service_account_key.json")
+if not firebase_admin._apps:
+    cred = credentials.Certificate("firebase_service_account_key.json")
+    firebase_admin.initialize_app(cred)
+
 firestore_db = firestore.client()
