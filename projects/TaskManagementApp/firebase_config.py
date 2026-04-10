d6df6323c38a386e9c1f0e9bbcafc54b283e0e45
jrichwiltshire <jrichwiltshire@gmail.com>
2026-04-09 19:36:19 -0500

    Update firebase_config.py since Railway doesn't have environment variables

--- projects/TaskManagementApp/firebase_config.py
+++ projects/TaskManagementApp/firebase_config.py
@@ -1,8 +1,14 @@
+import json
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_service_account_key.json")
+    service_account_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+    if service_account_json:
+        cred = credentials.Certificate(json.loads(service_account_json))
+    else:
+        cred = credentials.Certificate("firebase_service_account_key.json")
     firebase_admin.initialize_app(cred)
 
 firestore_db = firestore.client()
